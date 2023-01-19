3a1a0f7710bf404b0d555f7918c0104f3e6225b6
Rachael <rachael.rocha@mavs.uta.edu>
2023-01-19 17:07:46 -0600

    FIx

--- P01/bonus/Hello.java
+++ P01/bonus/Hello.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Hello {
     public static void main(String[] args) {
         Scanner userName = new Scanner(System.in);
-        System.out.println("Enter your Name: ");
+        System.out.print("Enter your Name: ");
 
         String name = userName.nextLine();
         System.out.println("Hello " + name);
