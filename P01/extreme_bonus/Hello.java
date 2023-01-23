8ebef425bfc66991cd1126a7e7d6fc2dba4546a9
Rachael <rachael.rocha@mavs.uta.edu>
2023-01-23 15:44:53 -0600

    final

--- P01/extreme_bonus/Hello.java
+++ P01/extreme_bonus/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
+        String username = System.getProperty("user.name");
+        System.out.println("Hello " + username);
 
     }
 }
