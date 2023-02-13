cb5f60ea49498821d4e7cb01410d19a95b477717
Rachael <rachael.rocha@mavs.uta.edu>
2023-02-13 14:56:51 -0600

    3

--- P04/Taxfree.java
+++ P04/Taxfree.java
@@ -1,3 +1,12 @@
-public class Taxfree {
+public class Taxfree extends Product {
+
+    public Taxfree(double cost, String name) {
+        super(cost, name);
+    }
+
+    @Override
+    public double price() {
+        return 3.0; // filler for now
+    }
 
 }
