0412d22f045e5e6a6ccba7361635f447337553f5
Rachael <rachael.rocha@mavs.uta.edu>
2023-02-13 17:11:50 -0600

    h

--- P04/Store.java
+++ P04/Store.java
@@ -1,4 +1,9 @@
+import java.util.ArrayList;
+
 public class Store {
+    private static ArrayList<Product> products = new ArrayList<Product>();
+    private static ArrayList<Product> shoppingCart = new ArrayList<Product>();
+
     public static void main(String[] args) {
 
     }
