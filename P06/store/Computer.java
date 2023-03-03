08ffe00f6250b39d969f0a3cbd44f0248823abdf
Rachael <rachael.rocha@mavs.uta.edu>
2023-03-02 19:57:33 -0600

    2

---
+++ P06/store/Computer.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+
+public class Computer {
+    private String name;
+    private String model;
+    private ArrayList<Option> options = new ArrayList<Option>();
+
+    public Computer(String name, String model) {
+        this.name = name;
+        this.model = model;
+    }
+
+    public void addOption(Option option) {
+        options.add(option);
+
+    }
+
+    public long cost() {
+        long totalCost = 0;
+        for (Option i : options) {
+            totalCost = totalCost + i.getCost();
+        }
+        return totalCost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public ArrayList<Option> getOptions() {
+        return options;
+    }
+
+    @Override
+    public String toString() {
+        // have to use string builder????
+        StringBuilder finalString = new StringBuilder();
+        // option will have cost and name
+        finalString.append(name + " (" + model + ")\n");
+        for (Option i : options) {
+            finalString.append(i.getName() + " (" + i.getCost() + ")\n");
+        }
+
+        return finalString.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this.toString().equals(o.toString())) {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ P06/store/Customer.java
@@ -0,0 +1,54 @@
+import java.util.regex.*;
+
+public class Customer {
+    String name;
+    private String email;
+
+    public Customer(String name, String email) {
+        this.email = email;
+        this.name = name;
+
+        if (!validateEmail(email)) {
+            throw new IllegalArgumentException("Invalid Email Syntax ex. name@domain.com");
+        }
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+
+    }
+
+    public static final Pattern regexEmail = Pattern.compile("^([a-zA-Z0-9_.-]+)@([a-zA-Z0-9_.-]+).([a-zA-Z]{2,5})$",
+            Pattern.CASE_INSENSITIVE);
+
+    public static boolean validateEmail(String email) {
+        Matcher matchedPattern = regexEmail.matcher(email);
+        return matchedPattern.matches();
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + email + ")\n";
+
+    }
+
+    // to make sure there's not multiple of same email??
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+
+        if (!(o instanceof Customer)) {
+            return false;
+        }
+
+        Customer otherCustomer = (Customer) o;
+        return this.email.equals(otherCustomer.email);
+    }
+}
