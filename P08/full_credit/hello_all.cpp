fb6fd33e13552ac12495e95de5267bba698173c1
Rachael <rachael.rocha@mavs.uta.edu>
2023-04-03 16:28:08 -0500

    full credit

---
+++ P08/full_credit/hello_all.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std:: string name;
+    std::cout <<"Please enter your name: ";
+    std::cin >> name;
+    std::cout << "Hello, " << name << "!" << std::endl;
+    return 0;
+}
---
+++ P08/full_credit/hello_me.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std:: string name;
+    std::cout <<"Hello Rachael!" << std::endl;
+    return 0;
+}
