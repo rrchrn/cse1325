195d1fe75d7bd6606e9eec3ccdb87e9e76cececc
Rachael <rachael.rocha@mavs.uta.edu>
2023-04-27 01:42:25 -0500

    ok

---
+++ P13/store/Computer.java
@@ -0,0 +1,76 @@
+package store;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Computer {
+    private String name;
+    private String model;
+    private ArrayList<Option> options = new ArrayList<>();
+    private boolean deprecated = false; // new field
+
+    public Computer(String name, String model) {
+        this.name = name;
+        this.model = model;
+    }
+
+    public Computer(BufferedReader br) throws IOException {
+        this.name = br.readLine();
+        this.model = br.readLine();
+        int numOptions = Integer.parseInt(br.readLine());
+        while (numOptions-- > 0)
+            options.add(new Option(br));
+    }
+
+    public void save(BufferedWriter bw) throws IOException {
+        bw.write(name + '\n');
+        bw.write(model + '\n');
+        bw.write("" + options.size() + '\n');
+        for (Option option : options)
+            option.save(bw);
+    }
+
+    public void addOption(Option option) {
+        options.add(option);
+    }
+
+    public long cost() {
+        long cost = 0;
+        for (Option o : options)
+            cost += o.cost();
+        return cost;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder(name + " (" + model + ")");
+        for (Option o : options)
+            sb.append("\n  " + o);
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        try {
+            if (this == o)
+                return true;
+            if (this.getClass() != o.getClass())
+                return false;
+            Computer c = (Computer) o;
+            return this.toString().equals(c.toString());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    // new getter and setter for deprecation field
+    public boolean isDeprecated() {
+        return deprecated;
+    }
+
+    public void setDeprecated(boolean deprecated) {
+        this.deprecated = deprecated;
+    }
+}
---
+++ P13/store/Store.java
@@ -0,0 +1,120 @@
+package store;
+
+import java.util.ArrayList;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+public class Store {
+    public Store(String name) {
+        this.name = name;
+    }
+
+    public Store(BufferedReader br) throws IOException {
+        this.name = br.readLine();
+        int numOptions = Integer.parseInt(br.readLine());
+        while (numOptions-- > 0)
+            customers.add(new Customer(br));
+
+        numOptions = Integer.parseInt(br.readLine());
+        while (numOptions-- > 0)
+            options.add(new Option(br));
+
+        numOptions = Integer.parseInt(br.readLine());
+        while (numOptions-- > 0)
+            computers.add(new Computer(br));
+
+        numOptions = Integer.parseInt(br.readLine());
+        while (numOptions-- > 0)
+            orders.add(new Order(br));
+    }
+
+    public void save(BufferedWriter bw) throws IOException {
+        bw.write(name + '\n');
+
+        bw.write("" + customers.size() + '\n');
+        for (Customer customer : customers)
+            customer.save(bw);
+
+        bw.write("" + options.size() + '\n');
+        for (Option option : options)
+            option.save(bw);
+
+        bw.write("" + computers.size() + '\n');
+        for (Computer computer : computers)
+            computer.save(bw);
+
+        bw.write("" + orders.size() + '\n');
+        for (Order order : orders)
+            order.save(bw);
+
+    }
+
+    public String name() {
+        return this.name;
+    }
+
+    // ///////////////////////////////////////////////////////////
+    // Customers
+
+    public void add(Customer customer) {
+        if (!customers.contains(customer))
+            customers.add(customer);
+    }
+
+    public Object[] customers() {
+        return this.customers.toArray();
+    }
+
+    // ///////////////////////////////////////////////////////////
+    // Options
+
+    public void add(Option option) {
+        if (!options.contains(option))
+            options.add(option);
+    }
+
+    public Object[] options() {
+        return this.options.toArray();
+    }
+
+    // ///////////////////////////////////////////////////////////
+    // Computers
+
+    public void add(Computer computer) {
+        if (!computers.contains(computer))
+            computers.add(computer);
+    }
+
+    public Computer[] computers() {
+        Set<Computer> nonDeprecatedComputers = new HashSet<>();
+        for (Computer computer : this.computers) {
+            if (!computer.isDeprecated()) {
+                nonDeprecatedComputers.add(computer);
+            }
+        }
+        return nonDeprecatedComputers.toArray(new Computer[0]);
+    }
+
+    // ///////////////////////////////////////////////////////////
+    // Orders
+
+    public void add(Order order) {
+        if (!orders.contains(order))
+            orders.add(order);
+    }
+
+    public Object[] orders() {
+        return this.orders.toArray();
+    }
+
+    // ///////////////////////////////////////////////////////////
+    // Fields
+
+    private String name;
+    private ArrayList<Customer> customers = new ArrayList<>();
+    private ArrayList<Option> options = new ArrayList<>();
+    private ArrayList<Computer> computers = new ArrayList<>();
+    private ArrayList<Order> orders = new ArrayList<>();
+}
